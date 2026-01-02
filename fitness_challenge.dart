d4b5e8f66b97945667d89dffeac0fabed2b6c851
3bdalmgeedw <3bdalmgeedw@gmail.com>
2026-01-02 19:28:33 +0200

    update with do while

--- fitness_challenge.dart
+++ fitness_challenge.dart
@@ -7,22 +7,29 @@ String? input = stdin.readLineSync();
 int goal = int.parse(input!);
 print("your goal is to do $goal push ups!");
 print("let's start the challenge!");
+int currentProgress = 0;
+
+do{
 print("enter your push ups done today:");
 String? input2 = stdin.readLineSync();
 int goal2 = int.parse(input2!);
-
-
-if(goal2<goal){
-  int remaining = goal - goal2;
+currentProgress += goal2;
+print("you have done $currentProgress push ups so far.");
+int remaining = goal - currentProgress;
+ if(currentProgress < goal){
+  int remaining = goal - currentProgress;
   print("you did $goal2 push ups today. you need $remaining more to reach your goal.");
-}else if (goal2 == goal){
+}else if (currentProgress == goal){
   print("congratulations! you reached your goal of $goal push ups!");
-}else if (goal2 > goal){
+}else if (currentProgress > goal){
   print("you did more than your goal amazing!");
 
 }else{
   print("you must rememmber your goal");
 
 }
+
+
+}while(currentProgress < goal);
 
 }
