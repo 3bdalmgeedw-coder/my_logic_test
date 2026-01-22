b77a66f2afeb8ae6c193ffd4b4f49ef7a12a0857
3bdalmgeedw <3bdalmgeedw@gmail.com>
2026-01-22 18:45:11 +0200

    update: optimize user exit condition in discount calculator

--- main.dart
+++ main.dart
@@ -3,13 +3,14 @@ import 'dart:io';
 void main(){
   double price =-1;
 print("--- Welcome to Discount Calculator ---");
-while (price !=0){
+while (true){
   print("\nENTER THE PRICE (or 0 to exit):");
   String? input=stdin.readLineSync();
   price = double.parse(input!);
 
   if(price==0){
     print("Closing program... Goodbye!");
+    break;
   }else if (price >=100){
     double off = price * 0.20;
     double finalPrice = price -off;
